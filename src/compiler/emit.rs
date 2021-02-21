100de57e4abbfa86453754addfd0a1f35988ae55
William McPherson <willmcpherson2@gmail.com>
2021-02-21 19:01:15 +1100

    Implement Display for Target and its children
    I think this is a bit nicer and should be more efficient.

--- src/compiler/emit.rs
+++ src/compiler/emit.rs
@@ -11,109 +11,137 @@ use super::symbol::Terminal;
 use std::fmt;
 
 pub fn emit(target: Target) -> String {
-    let mut code = String::new();
-    for def in &target.defs {
-        code.push_str(&emit_def(&def));
-    }
-    code
+    format!("{}", target)
 }
 
-fn emit_def(def: &Def) -> String {
-    format!(
-        "define {} @{}{}({}) {{\n{}}}\n",
-        def.ret,
-        def.name.name,
-        emit_id(def.name.id),
-        emit_params(&def.params),
-        emit_instructions(&def.instructions)
-    )
-}
-
-fn emit_params(params: &[Param]) -> String {
-    let mut code = String::new();
-    if let Some((first, rest)) = params.split_first() {
-        code.push_str(&format!("{} %{}", first.typ, first.id));
-        for param in rest {
-            code.push_str(&format!(", {} %{}", param.typ, param.id));
+impl fmt::Display for Target {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for def in &self.defs {
+            write!(f, "{}", def)?;
         }
+        Ok(())
     }
-    code
 }
 
-fn emit_args(args: &[Arg]) -> String {
-    let mut code = String::new();
-    if let Some((first, rest)) = args.split_first() {
-        code.push_str(&format!("{} {}", first.typ, first.data));
-        for arg in rest {
-            code.push_str(&format!(", {} {}", arg.typ, arg.data));
-        }
+impl fmt::Display for Def {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "define {} @{}({}) {{\n{}}}\n",
+            self.ret,
+            IdName(&self.name.name, self.name.id),
+            Params(&self.params),
+            Instructions(&self.instructions),
+        )
     }
-    code
 }
 
-fn emit_instructions(instructions: &[Instruction]) -> String {
-    let mut code = String::new();
-    for instruction in instructions {
-        code.push_str(&emit_instruction(instruction));
-    }
-    code
-}
+struct Params<'a>(&'a Vec<Param>);
 
-fn emit_instruction(instruction: &Instruction) -> String {
-    match instruction {
-        Instruction::Ret(ret) => {
-            if let Some(data) = &ret.data {
-                format!("ret {} {}\n", ret.typ, data)
-            } else {
-                format!("ret {}\n", ret.typ)
+impl fmt::Display for Params<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some((first, rest)) = self.0.split_first() {
+            write!(f, "{} %{}", first.typ, first.id)?;
+            for param in rest {
+                write!(f, ", {} %{}", param.typ, param.id)?;
             }
         }
-        Instruction::Call(call) => {
-            if let Some(out) = &call.out {
-                format!(
-                    "%{} = call {} @{}{}({})\n",
-                    out,
-                    call.typ,
-                    call.call_name.name,
-                    emit_id(call.call_name.id),
-                    emit_args(&call.args)
+        Ok(())
+    }
+}
+
+struct Args<'a>(&'a Vec<Arg>);
+
+impl fmt::Display for Args<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some((first, rest)) = self.0.split_first() {
+            write!(f, "{} {}", first.typ, first.data)?;
+            for arg in rest {
+                write!(f, ", {} {}", arg.typ, arg.data)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+struct Instructions<'a>(&'a Vec<Instruction>);
+
+impl fmt::Display for Instructions<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for instruction in self.0.iter() {
+            write!(f, "{}", instruction)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Instruction::Ret(ret) => {
+                if let Some(data) = &ret.data {
+                    write!(f, "ret {} {}\n", ret.typ, data)
+                } else {
+                    write!(f, "ret {}\n", ret.typ)
+                }
+            }
+            Instruction::Call(call) => {
+                if let Some(out) = &call.out {
+                    write!(
+                        f,
+                        "%{} = call {} @{}({})\n",
+                        out,
+                        call.typ,
+                        IdName(&call.call_name.name, call.call_name.id),
+                        Args(&call.args)
+                    )
+                } else {
+                    write!(
+                        f,
+                        "call {} @{}({})\n",
+                        call.typ,
+                        IdName(&call.call_name.name, call.call_name.id),
+                        Args(&call.args)
+                    )
+                }
+            }
+            Instruction::Unary(unary) => match unary.op {
+                UnaryOp::Not => {
+                    write!(
+                        f,
+                        "%{} = xor {} {}, true\n",
+                        unary.out, unary.typ, unary.arg,
+                    )
+                }
+                UnaryOp::BNot => {
+                    write!(f, "%{} = xor {} {}, -1\n", unary.out, unary.typ, unary.arg,)
+                }
+            },
+            Instruction::Binary(binary) => {
+                write!(
+                    f,
+                    "%{} = {} {} {}, {}\n",
+                    binary.out,
+                    Op(binary.op, binary.typ),
+                    binary.typ,
+                    binary.arg1,
+                    binary.arg2
                 )
-            } else {
-                format!(
-                    "call {} @{}{}({})\n",
-                    call.typ,
-                    call.call_name.name,
-                    emit_id(call.call_name.id),
-                    emit_args(&call.args)
-                )
             }
-        }
-        Instruction::Unary(unary) => match unary.op {
-            UnaryOp::Not => {
-                format!("%{} = xor {} {}, true\n", unary.out, unary.typ, unary.arg,)
-            }
-            UnaryOp::BNot => {
-                format!("%{} = xor {} {}, -1\n", unary.out, unary.typ, unary.arg,)
-            }
-        },
-        Instruction::Binary(binary) => {
-            format!(
-                "%{} = {} {} {}, {}\n",
-                binary.out,
-                emit_binary_op(binary.op, binary.typ),
-                binary.typ,
-                binary.arg1,
-                binary.arg2
-            )
         }
     }
 }
 
-fn emit_id(id: Id) -> String {
-    if id == 0 {
-        String::new()
-    } else {
-        format!("{}", id)
+struct IdName<'a>(&'a str, Id);
+
+impl fmt::Display for IdName<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.0)?;
+        if self.1 != 0 {
+            write!(f, "{}", self.1)
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -146,53 +174,56 @@ impl fmt::Display for Terminal {
     }
 }
 
-fn emit_binary_op(op: BinaryOp, typ: Terminal) -> String {
-    enum IntType {
-        Unsigned,
-        Signed,
-        Float,
-    }
+struct Op(BinaryOp, Terminal);
 
-    fn int_type(typ: Terminal) -> IntType {
-        match typ {
-            Terminal::I8 | Terminal::I16 | Terminal::I32 | Terminal::I64 => IntType::Signed,
-            Terminal::F16 | Terminal::F32 | Terminal::F64 => IntType::Float,
-            _ => IntType::Unsigned,
+impl fmt::Display for Op {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        enum IntType {
+            Unsigned,
+            Signed,
+            Float,
         }
-    }
 
-    let typ = int_type(typ);
-
-    macro_rules! instruction {
-        ($int_type:ident, $unsigned:literal, $signed:literal, $float:literal) => {
-            match $int_type {
-                IntType::Unsigned => $unsigned,
-                IntType::Signed => $signed,
-                IntType::Float => $float,
+        fn int_type(typ: Terminal) -> IntType {
+            match typ {
+                Terminal::I8 | Terminal::I16 | Terminal::I32 | Terminal::I64 => IntType::Signed,
+                Terminal::F16 | Terminal::F32 | Terminal::F64 => IntType::Float,
+                _ => IntType::Unsigned,
             }
-        };
-    }
+        }
 
-    match op {
-        BinaryOp::Add => instruction!(typ, "add", "add", "fadd"),
-        BinaryOp::Sub => instruction!(typ, "sub", "sub", "fsub"),
-        BinaryOp::Mul => instruction!(typ, "mul", "mul", "fmul"),
-        BinaryOp::Div => instruction!(typ, "udiv", "sdiv", "fdiv"),
-        BinaryOp::Rem => instruction!(typ, "urem", "srem", "frem"),
-        BinaryOp::BitAnd => instruction!(typ, "and", "and", "and"),
-        BinaryOp::BitOr => instruction!(typ, "or", "or", "or"),
-        BinaryOp::BitXor => instruction!(typ, "xor", "xor", "xor"),
-        BinaryOp::LShift => instruction!(typ, "shl", "shl", "shl"),
-        BinaryOp::RShift => instruction!(typ, "lshr", "lshr", "lshr"),
-        BinaryOp::And => instruction!(typ, "and", "and", "and"),
-        BinaryOp::Or => instruction!(typ, "or", "or", "or"),
-        BinaryOp::Xor => instruction!(typ, "xor", "xor", "xor"),
-        BinaryOp::Equal => instruction!(typ, "icmp eq", "icmp eq", "fcmp oeq"),
-        BinaryOp::NEqual => instruction!(typ, "icmp ne", "icmp ne", "fcmp une"),
-        BinaryOp::LEqual => instruction!(typ, "icmp ule", "icmp sle", "fcmp ole"),
-        BinaryOp::GEqual => instruction!(typ, "icmp uge", "icmp sge", "fcmp oge"),
-        BinaryOp::Less => instruction!(typ, "icmp ult", "icmp slt", "icmp olt"),
-        BinaryOp::Greater => instruction!(typ, "icmp ugt", "icmp sgt", "icmp ogt"),
+        let typ = int_type(self.1);
+
+        macro_rules! instruction {
+            ($int_type:ident, $unsigned:literal, $signed:literal, $float:literal) => {
+                match $int_type {
+                    IntType::Unsigned => $unsigned,
+                    IntType::Signed => $signed,
+                    IntType::Float => $float,
+                }
+            };
+        }
+
+        f.write_str(match self.0 {
+            BinaryOp::Add => instruction!(typ, "add", "add", "fadd"),
+            BinaryOp::Sub => instruction!(typ, "sub", "sub", "fsub"),
+            BinaryOp::Mul => instruction!(typ, "mul", "mul", "fmul"),
+            BinaryOp::Div => instruction!(typ, "udiv", "sdiv", "fdiv"),
+            BinaryOp::Rem => instruction!(typ, "urem", "srem", "frem"),
+            BinaryOp::BitAnd => instruction!(typ, "and", "and", "and"),
+            BinaryOp::BitOr => instruction!(typ, "or", "or", "or"),
+            BinaryOp::BitXor => instruction!(typ, "xor", "xor", "xor"),
+            BinaryOp::LShift => instruction!(typ, "shl", "shl", "shl"),
+            BinaryOp::RShift => instruction!(typ, "lshr", "lshr", "lshr"),
+            BinaryOp::And => instruction!(typ, "and", "and", "and"),
+            BinaryOp::Or => instruction!(typ, "or", "or", "or"),
+            BinaryOp::Xor => instruction!(typ, "xor", "xor", "xor"),
+            BinaryOp::Equal => instruction!(typ, "icmp eq", "icmp eq", "fcmp oeq"),
+            BinaryOp::NEqual => instruction!(typ, "icmp ne", "icmp ne", "fcmp une"),
+            BinaryOp::LEqual => instruction!(typ, "icmp ule", "icmp sle", "fcmp ole"),
+            BinaryOp::GEqual => instruction!(typ, "icmp uge", "icmp sge", "fcmp oge"),
+            BinaryOp::Less => instruction!(typ, "icmp ult", "icmp slt", "icmp olt"),
+            BinaryOp::Greater => instruction!(typ, "icmp ugt", "icmp sgt", "icmp ogt"),
+        })
     }
-    .to_string()
 }
